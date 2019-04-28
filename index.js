bebffcbcb3bddb1bb70cb5a1d7b459b9e9585732
TaphieM <taffymayen@gmail.com>
2019-04-27 23:03:39 -0500

    se le agrego a la función readFileInfo las opciones de validate, sin embargo necesita mejoras

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node                             //esta linea sirve para indicar que es un programa JS
 //De aquí exporto la función md-links e imprimo en consola 
 
-const readFile=require("./src/md-links");
+const readFileInfo=require("./src/md-links");
 
 
--- src/md-links.js
+++ src/md-links.js
@@ -1,40 +1,44 @@
 
+/*Librerias que usamos de node.js*/
 const fs = require('fs');   
 const fetch = require('node-fetch');
 const markdownLinkExtractor = require('markdown-link-extractor')
 const pathLibrary = require ('path')
 
 
-//Función que va a leer la ruta ingresada por el usuario e identificar si es un md
- const readFile = (path,type)=>{   
+/*Función que va a leer la ruta ingresada por el usuario e identificar si es un md*/
+ const readFileInfo = (path,type)=>{   
     return new Promise ((resolve,reject)=>{
      fs.readFile(path,type, (error,content) =>{
-         pathLibrary.extname(path)!='.md' ? reject(error) : resolve(content); //condición para que solo lea los archivos .md
-      });
-      
+         pathLibrary.extname(path)!='.md' ? reject(error) : resolve(content); /*condición para que solo lea los archivos .md*/
+      });      
     });
-    
   } 
-  readFile(process.argv[2], 'utf-8')  //llamamos la función, el primer parametro es la ruta y el segundo el código para que no lo muestre en binario
+
+  readFileInfo(process.argv[2], 'utf-8')  /*llamamos la función, el primer parametro es la ruta y el segundo el código para que no lo muestre en binario*/
   .then(res => {
       
-       let links = markdownLinkExtractor(res) //extrae los links
-       let linkInfo = [];                     //arreglo vacío que contendra todos los links con la informaciñon requerida
+       let links = markdownLinkExtractor(res) /*extrae los links*/
+       let linkInfo = [];                     /*arreglo vacío que contendra todos los links con la informaciñon requerida*/
 
-       links.forEach((link)=>{                //recorremos cada link
-       fetch(link)                            //extraemos información de cada link
+       links.forEach((link)=>{                /*recorremos cada link*/
+       fetch(link)                            /*extraemos información de cada link*/
          .then((links) =>{
-          linkInfo.push({                     //con el push generamos un objeto con las key requeridas en el readme
+          linkInfo.push({                     /*con el push generamos un objeto con las key requeridas en el readme*/
             href: link[0],
             text: link[1].substr(0,50),
-            file: process.argv[2]
+            file: process.argv[2],
+            status: links.status,
+            statusText: links.statusText
           })
-          console.log(linkInfo);              //imprimimos en consola el resultado
-          //console.log(res.url + ' ' +res.status+ ' ' +res.statusText);
-
+          //console.log('todo igual')
+          console.log(linkInfo);              /*imprimimos en consola el resultado*/
+          //console.log(links);              /*imprimimos en consola el resultado*/
+  
       }) 
         .catch(error =>{
           console.log(error.message)
+          //reject(error)
       })
       
   });
@@ -46,3 +50,17 @@ const pathLibrary = require ('path')
   
 
 
+
+
+
+
+
+
+
+
+//validateLinkStatus();
+
+
+module.exports={
+  readFileInfo
+}
