172d72b720520aceb6060666777a8eeee61758bc
TaphieM <taffymayen@gmail.com>
2019-04-27 19:39:00 -0500

    la función readFile ya arroja un arreglo de objetos

--- src/md-links.js
+++ src/md-links.js
@@ -4,28 +4,36 @@ const fetch = require('node-fetch');
 const markdownLinkExtractor = require('markdown-link-extractor')
 const pathLibrary = require ('path')
 
+
 //Función que va a leer la ruta ingresada por el usuario e identificar si es un md
  const readFile = (path,type)=>{   
     return new Promise ((resolve,reject)=>{
      fs.readFile(path,type, (error,content) =>{
-         //let error =console.log('Porfavor ingresa un archivo .md')
-         pathLibrary.extname(path)!='.md' ? reject(error) : resolve(content);
+         pathLibrary.extname(path)!='.md' ? reject(error) : resolve(content); //condición para que solo lea los archivos .md
       });
       
     });
     
-  }
-  
- readFile(process.argv[2], 'utf-8')
+  } 
+  readFile(process.argv[2], 'utf-8')  //llamamos la función, el primer parametro es la ruta y el segundo el código para que no lo muestre en binario
   .then(res => {
-    //console.log(res)
-     let links = markdownLinkExtractor(res)
-    links.forEach((link)=>{
-      fetch(link).then((res) =>{
-          //console.log(link);
-          console.log(res.url + ' ' +res.status+ ' ' +res.statusText);
+      
+       let links = markdownLinkExtractor(res) //extrae los links
+       let linkInfo = [];                     //arreglo vacío que contendra todos los links con la informaciñon requerida
+
+       links.forEach((link)=>{                //recorremos cada link
+       fetch(link)                            //extraemos información de cada link
+         .then((links) =>{
+          linkInfo.push({                     //con el push generamos un objeto con las key requeridas en el readme
+            href: link[0],
+            text: link[1].substr(0,50),
+            file: process.argv[2]
+          })
+          console.log(linkInfo);              //imprimimos en consola el resultado
+          //console.log(res.url + ' ' +res.status+ ' ' +res.statusText);
+
       }) 
-      .catch(error =>{
+        .catch(error =>{
           console.log(error.message)
       })
       
@@ -33,7 +41,7 @@ const pathLibrary = require ('path')
   })
  
   .catch(error =>{
-    console.log('Por favor ingresa un archivo .md')
+    console.log('Por favor ingresa un archivo .md') 
 })
   
 
